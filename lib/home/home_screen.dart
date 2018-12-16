387bca9558b5605db686ccd2c15e9ddf2fa6581a
LvYang <tylvyang@qq.com>
2018-12-16 20:54:45 +0800

    06-

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,92 +1,93 @@
 import 'package:flutter/material.dart';
 import 'package:wechat/constants.dart';
 
-enum ActionItems {
-  GROUP_CHAT, ADD_FRIEND, QR_SCAN, PAYMENT, HELP
-}
+enum ActionItems { GROUP_CHAT, ADD_FRIEND, QR_SCAN, PAYMENT, HELP }
 
 class NavigationIconView {
   final BottomNavigationBarItem item;
-  NavigationIconView({String title, IconData icon, IconData activeIcon}) :
-      item = BottomNavigationBarItem(
+  NavigationIconView({String title, IconData icon, IconData activeIcon})
+      : item = BottomNavigationBarItem(
           icon: Icon(icon),
           activeIcon: Icon(activeIcon),
           title: Text(title),
           backgroundColor: Colors.white,
-      );
+        );
 }
 
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
-
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  PageController _pageController;
   int _currentIndex = 0;
   List<NavigationIconView> _navigationViews;
+  List<Widget> _pages;
 
-  void initState(){
+  void initState() {
     super.initState();
     _navigationViews = [
       NavigationIconView(
-        title: '微信',
-        icon: IconData(
-          0xe608,
-          fontFamily: Constants.IconFontFamily,
-        ),
+          title: '微信',
+          icon: IconData(
+            0xe608,
+            fontFamily: Constants.IconFontFamily,
+          ),
           activeIcon: IconData(
             0xe608,
             fontFamily: Constants.IconFontFamily,
-          )
-
-      ),
+          )),
       NavigationIconView(
-        title: '通讯录',
-        icon: IconData(
-          0xe656,
-          fontFamily: Constants.IconFontFamily,
-        ),
+          title: '通讯录',
+          icon: IconData(
+            0xe656,
+            fontFamily: Constants.IconFontFamily,
+          ),
           activeIcon: IconData(
             0xe656,
             fontFamily: Constants.IconFontFamily,
-          )
-      ),
+          )),
       NavigationIconView(
-        title: '发现',
-        icon: IconData(
-          0xe671,
-          fontFamily: Constants.IconFontFamily,
-        ),
+          title: '发现',
+          icon: IconData(
+            0xe671,
+            fontFamily: Constants.IconFontFamily,
+          ),
           activeIcon: IconData(
             0xe671,
             fontFamily: Constants.IconFontFamily,
-          )
-      ),
+          )),
       NavigationIconView(
-        title: '我',
-        icon: IconData(
-          0xe6c0,
-          fontFamily: Constants.IconFontFamily,
-        ),
-        activeIcon: IconData(
-          0xe6c0,
-          fontFamily: Constants.IconFontFamily,
-        )
-      ),
+          title: '我',
+          icon: IconData(
+            0xe6c0,
+            fontFamily: Constants.IconFontFamily,
+          ),
+          activeIcon: IconData(
+            0xe6c0,
+            fontFamily: Constants.IconFontFamily,
+          )),
+    ];
+    _pageController = PageController(initialPage: _currentIndex);
+    _pages = [
+      Container(color: Colors.red),
+      Container(color: Colors.green),
+      Container(color: Colors.blue),
+      Container(color: Colors.brown),
     ];
   }
 
-  _buildPopupMunuItem(int iconName, String title){
+  _buildPopupMunuItem(int iconName, String title) {
     return Row(
-        children: <Widget>[
-            Icon(IconData(
-              iconName,
-              fontFamily: Constants.IconFontFamily
-            ), size: 22.0, color: const Color(AppColors.AppBarPopuMenuColor)),
-            Container(width: 12.0),
-            Text(title, style: TextStyle(color: const Color(AppColors.AppBarPopuMenuColor))),
-          ],
+      children: <Widget>[
+        Icon(IconData(iconName, fontFamily: Constants.IconFontFamily),
+            size: 22.0, color: const Color(AppColors.AppBarPopuMenuColor)),
+        Container(width: 12.0),
+        Text(title,
+            style:
+                TextStyle(color: const Color(AppColors.AppBarPopuMenuColor))),
+      ],
     );
   }
 
@@ -94,18 +95,19 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final BottomNavigationBar botNavBar = BottomNavigationBar(
       fixedColor: const Color(AppColors.TabIconActive),
-      items: _navigationViews.map((NavigationIconView view){
+      items: _navigationViews.map((NavigationIconView view) {
         return view.item;
       }).toList(),
       currentIndex: _currentIndex,
       type: BottomNavigationBarType.fixed,
-      onTap: (int index){
+      onTap: (int index) {
         setState(() {
           _currentIndex = index;
+          _pageController.animateToPage(_currentIndex,
+              duration: Duration(milliseconds: 200), curve: Curves.easeInOut);
         });
         print('点击的是第$index个Tab');
       },
-
     );
     return Scaffold(
       appBar: AppBar(
@@ -113,15 +115,22 @@ class _HomeScreenState extends State<HomeScreen> {
         elevation: 0.5,
         actions: [
           IconButton(
-            icon: Icon(IconData(
-              0xe65e,
-              fontFamily: Constants.IconFontFamily,
-            ), size: 22.0,),
-            onPressed: (){print("点击了搜索按钮");},
+            icon: Icon(
+              IconData(
+                0xe65e,
+                fontFamily: Constants.IconFontFamily,
+              ),
+              size: 22.0,
+            ),
+            onPressed: () {
+              print("点击了搜索按钮");
+            },
           ),
-          Container(width: 16.0,),
+          Container(
+            width: 16.0,
+          ),
           PopupMenuButton(
-            itemBuilder: (BuildContext context){
+            itemBuilder: (BuildContext context) {
               return <PopupMenuItem<ActionItems>>[
                 PopupMenuItem(
                   child: _buildPopupMunuItem(0xe69e, "发起群聊"),
@@ -145,19 +154,33 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ];
             },
-            icon: Icon(IconData(
-              0xe658,
-              fontFamily: Constants.IconFontFamily,
-            ), size: 22.0,),
-            onSelected: (ActionItems selected){
+            icon: Icon(
+              IconData(
+                0xe658,
+                fontFamily: Constants.IconFontFamily,
+              ),
+              size: 22.0,
+            ),
+            onSelected: (ActionItems selected) {
               print("点击的是$selected");
             },
           ),
-          Container(width: 16.0,)
+          Container(
+            width: 16.0,
+          )
         ],
       ),
-      body: Container(
-        color: Colors.redAccent,
+      body: PageView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return _pages[index];
+        },
+        controller: _pageController,
+        itemCount: _pages.length,
+        onPageChanged: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
       bottomNavigationBar: botNavBar,
     );
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,9 @@ import 'constants.dart' show AppColors;
 import './home/home_screen.dart';
 
 void main() => runApp(MaterialApp(
-  title: '微信',
-  theme: ThemeData.light().copyWith(
-      primaryColor: Color(AppColors.AppBarColor),
-      cardColor: Color(AppColors.AppBarColor)
-    ),
-  home: HomeScreen(),
-));
-
+      title: '微信',
+      theme: ThemeData.light().copyWith(
+          primaryColor: Color(AppColors.AppBarColor),
+          cardColor: Color(AppColors.AppBarColor)),
+      home: HomeScreen(),
+    ));
