5ba4ad0717b591d9b4bd9a30caeaf812e4681a3d
LvYang <tylvyang@qq.com>
2018-12-16 23:31:11 +0800

    07-1 替换微信页面为conversation_page,灰色页面.

---
+++ lib/home/conversation_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConversationPage extends StatefulWidget {
+  @override
+  _ConversationPageState createState() => _ConversationPageState();
+}
+
+class _ConversationPageState extends State<ConversationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+    );
+  }
+}
