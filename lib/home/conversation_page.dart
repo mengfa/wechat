f1c46e649d9f8dea9cee8f4b1005b5844459d90a
LvYang <tylvyang@qq.com>
2018-12-22 23:23:50 +0800

    1.完成微信会话列表首页图片的加载；

--- lib/home/conversation_page.dart
+++ lib/home/conversation_page.dart
@@ -7,47 +7,114 @@ class _ConversationItem extends StatelessWidget {
   const _ConversationItem({Key key, this.conversation})
       : assert(conversation != null),
         super(key: key);
+
   final Conversation conversation;
 
   @override
   Widget build(BuildContext context) {
+    Widget avatar;
+    if (conversation.isAvatarFromNet()) {
+      avatar = Image.network(
+        conversation.avatar,
+        width: Constants.ConversationAvatarSize,
+        height: Constants.ConversationAvatarSize,
+      );
+    } else {
+      avatar = Image.asset(
+        conversation.avatar,
+        width: Constants.ConversationAvatarSize,
+        height: Constants.ConversationAvatarSize,
+      );
+    }
+
+    Widget avatarContainer;
+    if (conversation.unreadMsgCount > 0) {
+      // 未读消息角标
+      Widget unreadMsgCountText = Container(
+        width: Constants.UnReadMsgNotifyDotSize,
+        height: Constants.UnReadMsgNotifyDotSize,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius:
+              BorderRadius.circular(Constants.UnReadMsgNotifyDotSize / 2.0),
+          color: Color(AppColors.NotifyDotBg),
+        ),
+        child: Text(conversation.unreadMsgCount.toString(),
+            style: AppStyles.UnreadMsgCountDotStyle),
+      );
+
+      avatarContainer = Stack(
+        overflow: Overflow.visible,
+        children: <Widget>[
+          avatar,
+          Positioned(
+            right: -6.0,
+            top: -6.0,
+            child: unreadMsgCountText,
+          )
+        ],
+      );
+    } else {
+      avatarContainer = avatar;
+    }
+
+    // 勿扰模式图标
+    var _rightArea = <Widget>[
+      Text(conversation.updateAt, style: AppStyles.DesStyle),
+      SizedBox(height: 10.0),
+    ];
+    if (conversation.isMute) {
+      _rightArea.add(Icon(
+          IconData(
+            0xe755,
+            fontFamily: Constants.IconFontFamily,
+          ),
+          color: Color(AppColors.ConversationMuteIcon),
+          size: Constants.ConversationMuteIconSize));
+    } else {
+      _rightArea.add(Icon(
+          IconData(
+            0xe755,
+            fontFamily: Constants.IconFontFamily,
+          ),
+          color: Colors.transparent,
+          size: Constants.ConversationMuteIconSize));
+    }
+
     return Container(
       padding: const EdgeInsets.all(10.0),
       decoration: BoxDecoration(
-        color: Color(AppColors.ConversationItemBg),
-        border: Border(
-          bottom: BorderSide(color: Color(AppColors.DividerColor),
-          width: Constants.DividerWidth
-          ),
-        )
-      ),
+          color: Color(AppColors.ConversationItemBg),
+          border: Border(
+            bottom: BorderSide(
+                color: Color(AppColors.DividerColor),
+                width: Constants.DividerWidth),
+          )),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Image.asset(
-              'assets/images/default_nor_avatar.png',
-            width: Constants.ConversationAvatarSize,
-            height: Constants.ConversationAvatarSize,
+          avatarContainer,
+          Container(
+            width: 10.0,
           ),
-          Container(width: 10.0,),
-          Expanded(child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(conversation.title, style: AppStyles.TitleStyle),
-              Text(conversation.des, style: AppStyles.DesStyle)
-            ],
-          )),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(conversation.title, style: AppStyles.TitleStyle),
+                Text(conversation.des, style: AppStyles.DesStyle)
+              ],
+            ),
+          ),
+          Container(width: 10.0),
           Column(
-            children: <Widget>[
-              Text('17.56',style: AppStyles.DesStyle)
-            ],
+            children: _rightArea,
           )
         ],
       ),
     );
   }
 }
-
 
 class ConversationPage extends StatefulWidget {
   @override
@@ -57,9 +124,8 @@ class ConversationPage extends StatefulWidget {
 class _ConversationPageState extends State<ConversationPage> {
   @override
   Widget build(BuildContext context) {
-
     return ListView.builder(
-      itemBuilder: (BuildContext context, int index){
+      itemBuilder: (BuildContext context, int index) {
         return _ConversationItem(conversation: mockConversations[index]);
       },
       itemCount: mockConversations.length,
--- lib/modal/conversation.dart
+++ lib/modal/conversation.dart
@@ -24,6 +24,14 @@ class Conversation {
   final bool isMute;
   final int unreadMsgCount;
   final bool displayDot;
+
+  bool isAvatarFromNet() {
+    if(this.avatar.indexOf('http') == 0 ||
+        this.avatar.indexOf('https') == 0) {
+      return true;
+    }
+    return false;
+  }
 }
 
 const List<Conversation> mockConversations = [
