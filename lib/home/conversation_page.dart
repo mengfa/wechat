a05fd6a259587dfa6cdab911b9ba33a884d2f704
LvYang <tylvyang@qq.com>
2018-12-22 17:19:47 +0800

    g 新增开发用到的图片资源； 新增会话列表界面的基本实现；

--- lib/home/conversation_page.dart
+++ lib/home/conversation_page.dart
@@ -1,5 +1,54 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart' show AppColors, AppStyles, Constants;
+import '../modal/conversation.dart' show Conversation, mockConversations;
+
+class _ConversationItem extends StatelessWidget {
+  const _ConversationItem({Key key, this.conversation})
+      : assert(conversation != null),
+        super(key: key);
+  final Conversation conversation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: Color(AppColors.ConversationItemBg),
+        border: Border(
+          bottom: BorderSide(color: Color(AppColors.DividerColor),
+          width: Constants.DividerWidth
+          ),
+        )
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(
+              'assets/images/default_nor_avatar.png',
+            width: Constants.ConversationAvatarSize,
+            height: Constants.ConversationAvatarSize,
+          ),
+          Container(width: 10.0,),
+          Expanded(child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(conversation.title, style: AppStyles.TitleStyle),
+              Text(conversation.des, style: AppStyles.DesStyle)
+            ],
+          )),
+          Column(
+            children: <Widget>[
+              Text('17.56',style: AppStyles.DesStyle)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
 class ConversationPage extends StatefulWidget {
   @override
   _ConversationPageState createState() => _ConversationPageState();
@@ -8,8 +57,12 @@ class ConversationPage extends StatefulWidget {
 class _ConversationPageState extends State<ConversationPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey,
+
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index){
+        return _ConversationItem(conversation: mockConversations[index]);
+      },
+      itemCount: mockConversations.length,
     );
   }
 }
