f76eab5c88e826ea4e4d07366c574bdbb32f07f5
LvYang <tylvyang@qq.com>
2019-01-01 01:14:42 +0800

    新增获取联系人位置的功能，增加IndexBar和ListView滚动位置互动的功能.

--- lib/home/contacts_page.dart
+++ lib/home/contacts_page.dart
@@ -16,15 +16,27 @@ class _ContactItem extends StatelessWidget {
   final String groupTitle;
   final VoidCallback onPressed;
 
+  static const double MARGIN_VERTICAL = 10.0;
+  static const double GROUP_TITLE_HEIGHT = 24.0;
+
   bool get _isAvatarFromNet {
     return this.avatar.startsWith('http') || this.avatar.startsWith('https');
+  }
+
+  static double _height(bool hasGroupTitle) {
+    final _buttonHeight = MARGIN_VERTICAL * 2 +
+        Constants.ContactAvatarSize + Constants.DividerWidth;
+    if(hasGroupTitle) {
+      return _buttonHeight + GROUP_TITLE_HEIGHT;
+    }
+    return _buttonHeight;
   }
 
   @override
   Widget build(BuildContext context) {
     // 左边的图标
     Widget _avatarIcon;
-    if (_isAvatarFromNet) {
+    if(_isAvatarFromNet) {
       _avatarIcon = Image.network(
         avatar,
         width: Constants.ContactAvatarSize,
@@ -41,11 +53,10 @@ class _ContactItem extends StatelessWidget {
     // 列表项主体部分
     Widget _button = Container(
       margin: const EdgeInsets.symmetric(horizontal: 16.0),
-      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      padding: const EdgeInsets.symmetric(vertical: MARGIN_VERTICAL),
       decoration: BoxDecoration(
         border: Border(
-          bottom: BorderSide(
-              width: Constants.DividerWidth,
+          bottom: BorderSide(width: Constants.DividerWidth,
               color: const Color(AppColors.DividerColor)),
         ),
       ),
@@ -60,16 +71,15 @@ class _ContactItem extends StatelessWidget {
 
     // 分组标签
     Widget _itemBody;
-    if (this.groupTitle != null) {
+    if(this.groupTitle != null) {
       _itemBody = Column(
         children: <Widget>[
           Container(
-            padding:
-                EdgeInsets.only(left: 16.0, right: 16.0, top: 4.0, bottom: 4.0),
+            height: GROUP_TITLE_HEIGHT,
+            padding: EdgeInsets.only(left: 16.0, right: 16.0),
             color: const Color(AppColors.ContactGroupTitleBg),
             alignment: Alignment.centerLeft,
-            child:
-                Text(this.groupTitle, style: AppStyles.GroupTitleItemTextStyle),
+            child: Text(this.groupTitle, style: AppStyles.GroupTitleItemTextStyle),
           ),
           _button,
         ],
@@ -83,79 +93,71 @@ class _ContactItem extends StatelessWidget {
 }
 
 const INDEX_BAR_WORDS = [
-  "↑",
-  "☆",
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z"
+  "↑", "☆",
+  "A", "B", "C", "D", "E", "F", "G",
+  "H", "I", "J", "K", "L", "M", "N",
+  "O", "P", "Q", "R", "S", "T", "U",
+  "V", "W", "X", "Y", "Z"
 ];
 
 class ContactsPage extends StatefulWidget {
+  Color _indexBarBg = Colors.transparent;
+
   _ContactsPageState createState() => _ContactsPageState();
 }
 
 class _ContactsPageState extends State<ContactsPage> {
+  ScrollController _scrollController;
   final ContactsPageData data = ContactsPageData.mock();
   final List<Contact> _contacts = [];
   final List<_ContactItem> _functionButtons = [
     _ContactItem(
         avatar: 'assets/images/ic_new_friend.png',
         title: '新的朋友',
-        onPressed: () {
-          print('添加新朋友。');
-        }),
+        onPressed: () { print('添加新朋友。'); }
+    ),
     _ContactItem(
         avatar: 'assets/images/ic_group_chat.png',
         title: '群聊',
-        onPressed: () {
-          print('点击了群聊。');
-        }),
+        onPressed: () { print('点击了群聊。'); }
+    ),
     _ContactItem(
         avatar: 'assets/images/ic_tag.png',
         title: '标签',
-        onPressed: () {
-          print('标签。');
-        }),
+        onPressed: () { print('标签。'); }
+    ),
     _ContactItem(
         avatar: 'assets/images/ic_public_account.png',
         title: '公众号',
-        onPressed: () {
-          print('添加公众号。');
-        }),
+        onPressed: () { print('添加公众号。'); }
+    ),
   ];
+  final Map _letterPosMap = {INDEX_BAR_WORDS[0]: 0.0};
 
   @override
   void initState() {
     super.initState();
-    _contacts
-      ..addAll(data.contacts)
-      ..addAll(data.contacts)
-      ..addAll(data.contacts);
-    _contacts
-        .sort((Contact a, Contact b) => a.nameIndex.compareTo(b.nameIndex));
+    _contacts..addAll(data.contacts)..addAll(data.contacts)..addAll(data.contacts);
+    _contacts.sort((Contact a, Contact b) => a.nameIndex.compareTo(b.nameIndex));
+    _scrollController = new ScrollController();
+    // 计算用于 IndexBar 进行定位的关键通讯录列表项的位置
+    var _totalPos = _functionButtons.length * _ContactItem._height(false);
+    for(int i = 0; i < _contacts.length; i++) {
+      bool _hasGroupTitle = true;
+      if(i > 0 && _contacts[i].nameIndex.compareTo(_contacts[i - 1].nameIndex) == 0) {
+        _hasGroupTitle = false;
+      }
+      if(_hasGroupTitle) {
+        _letterPosMap[_contacts[i].nameIndex] = _totalPos;
+      }
+      _totalPos += _ContactItem._height(_hasGroupTitle);
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
@@ -169,21 +171,22 @@ class _ContactsPageState extends State<ContactsPage> {
     return Stack(
       children: <Widget>[
         ListView.builder(
+          controller: _scrollController,
           itemBuilder: (BuildContext context, int index) {
-            if (index < _functionButtons.length) {
+            if(index < _functionButtons.length) {
               return _functionButtons[index];
             }
             int _contactIndex = index - _functionButtons.length;
             bool _isGroupTitle = true;
             Contact _contact = _contacts[_contactIndex];
-            if (_contactIndex >= 1 &&
-                _contact.nameIndex == _contacts[_contactIndex - 1].nameIndex) {
+            if(_contactIndex >= 1 && _contact.nameIndex == _contacts[_contactIndex - 1].nameIndex) {
               _isGroupTitle = false;
             }
-            return _ContactItem(
+            return  _ContactItem(
                 avatar: _contact.avatar,
                 title: _contact.name,
-                groupTitle: _isGroupTitle ? _contact.nameIndex : null);
+                groupTitle: _isGroupTitle ? _contact.nameIndex : null
+            );
           },
           itemCount: _contacts.length + _functionButtons.length,
         ),
@@ -192,8 +195,30 @@ class _ContactsPageState extends State<ContactsPage> {
           right: 0.0,
           top: 0.0,
           bottom: 0.0,
-          child: Column(
-            children: _letters,
+          child: Container(
+            color: widget._indexBarBg,
+            child: GestureDetector(
+              onVerticalDragDown: (DragDownDetails details) {
+                setState(() {
+                  widget._indexBarBg = Colors.black26;
+                });
+                _scrollController.animateTo(_letterPosMap['D'],
+                    curve: Curves.easeIn, duration: Duration(milliseconds: 200));
+              },
+              onVerticalDragEnd: (DragEndDetails details) {
+                setState(() {
+                  widget._indexBarBg = Colors.transparent;
+                });
+              },
+              onVerticalDragCancel: () {
+                setState(() {
+                  widget._indexBarBg = Colors.transparent;
+                });
+              },
+              child: Column(
+                children: _letters,
+              ),
+            ),
           ),
         )
       ],
